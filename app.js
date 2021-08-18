8c4d5544702e0dfa92cff8578647e282cf605ecf
JulieBarona <75625047+JulieBarona@users.noreply.github.com>
2021-08-17 23:58:56 -0400

    update index.html
    Updated description boxes with accurate info.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const date = require(__dirname);
-
 const app = express();
 
 app.set('view engine', 'ejs');
